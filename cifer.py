ac8e11ab57beabe9f166983305f18a26b3078ef7
tr5 <ad@lj>
2016-03-21 08:54:41 +0300

    #feature добавлено шифрование.

--- cifer.py
+++ cifer.py
@@ -1,7 +1,29 @@
 def encrypt(data, key):
-    # TODO: implement
-    return data
+    index = 0
+    r = []
+    kb = bytearray(key.encode())
+
+    for byte in data:
+        a = int(byte)
+        p = int(kb[index])
+        index = index + 1 if index + 1 < len(kb) else 0
+        r.append((a + p) % 256)
+
+    return bytes(r)
+
 
 def decrypt(data, key):
-    #TODO: implement
-    return data
+
+    index = 0
+    r = []
+    kb = bytearray(key.encode())
+
+    for byte in data:
+        b = int(byte)
+        p = int(kb[index])
+        index = index + 1 if index + 1 < len(kb) else 0
+
+        d = b - p
+        r.append(d if d >= 0 else d + 256)
+
+    return bytes(r)
